4afb425d485585f645b96f46891fe19037f01d59
TanyaHarutyunyan <tanyaharutyunyan01@gmail.com>
2025-05-16 21:57:34 +0400

    Install webpack dev server, add dev script, setup dev server configs

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,15 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     mode: "production",
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, "dist"),
+        },
+        port: 3000,
+        compress: true,
+        hot: true,
+        open: true,
+    },
     entry: path.resolve(__dirname, "./src/index.js"),
     output: {
         path: path.resolve(__dirname, "./dist"),
